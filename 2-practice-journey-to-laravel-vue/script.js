8c3c113103c3f50a2ce3f171598dbe1da5099c18
Kamrul Hasan Dipta <diptakamrulhasan437@gmail.com>
2022-12-08 22:21:15 +0600

    assigned variable

---
+++ 2-practice-journey-to-laravel-vue/script.js
@@ -0,0 +1,15 @@
+// First Class
+console.log("Kamrul Hasan Dipta")
+
+// Second Class
+// We can store variable by 
+// 1) var, 2) let and 3) const
+// Data Type Two types
+// 1) Premitive Data Type
+// 2) Non-Premitive Data Type
+
+var kamrulHasan = 10;
+console.log(kamrulHasan);
+
+var kamrulHasan = "dipta";
+console.log(kamrulHasan);
