951147c37f2136187b1960b3f88c4b39fc5cf39b
Kamrul Hasan Dipta <diptakamrulhasan437@gmail.com>
2022-12-12 11:39:06 +0600

    added operator

--- 2-practice-journey-to-laravel-vue/script.js
+++ 2-practice-journey-to-laravel-vue/script.js
@@ -13,3 +13,29 @@ console.log(kamrulHasan);
 
 var kamrulHasan = "dipta";
 console.log(kamrulHasan);
+
+// Third Class
+// Operators are many types
+// 1) Arithmatic Operator (+,-,*,/)
+// 2) Comparison Operator
+// 3) Assignment Operator
+// 4) Logical Operator
+// 5) Conditional Operator
+// 6) Modulo Operator or Remainder Operator
+// 7) Increment and Decrement Operator
+
+var a = 20;
+var b = 35;
+console.log(a + b);
+var c = 45;
+var d = 55;
+console.log(c - d);
+var p = 56;
+var n = 60;
+console.log(p * n);
+console.log(p / n);
+console.log(p != n);
+console.log(p == n);
+console.log(p === n);
+console.log(p >= n);
+console.log(p % n);
