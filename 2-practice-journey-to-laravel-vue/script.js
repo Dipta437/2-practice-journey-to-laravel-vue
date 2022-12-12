c99afe9ac30c111df879a64da6d0af00df56b73a
Kamrul Hasan Dipta <diptakamrulhasan437@gmail.com>
2022-12-12 20:03:35 +0600

    done with the switch case

--- 2-practice-journey-to-laravel-vue/script.js
+++ 2-practice-journey-to-laravel-vue/script.js
@@ -51,3 +51,36 @@ if (test == 5){
 }{
     console.log("Not correct");
 }
+
+// Fifth Class
+// Switch Statement
+
+var number = parseInt(prompt("Enter a number between 1 to 7 : "));
+
+switch (number) {
+    case 1:
+        console.log("Sunday");
+        break;
+        case 2:
+        console.log("Monday");
+        break;
+        case 3:
+        console.log("Tuesday");
+        break;
+        case 4:
+        console.log("Wednesday");
+        break;
+        case 5:
+        console.log("Thursday");
+        break;
+        case 6:
+        console.log("Friday");
+        break;
+        case 7:
+        console.log("Saturday");
+        break;
+
+    default:
+        console.log("Invalid Value");
+        
+}
